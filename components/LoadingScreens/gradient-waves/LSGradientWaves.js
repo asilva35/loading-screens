eff13507d4db07e0fb4d63a38441070ae46a0824
asilva35 <sistev.contacto@gmail.com>
2023-06-03 14:46:23 -0500

    Close Loading Screen

--- components/LoadingScreens/gradient-waves/LSGradientWaves.js
+++ components/LoadingScreens/gradient-waves/LSGradientWaves.js
@@ -29,27 +29,27 @@ export default function LSGradientScreen({ image }) {
   //     }
   //   }, []);
 
-  //   useEffect(() => {
-  //     const onPageLoad = () => {
-  //       const worker = new Worker(
-  //         new URL('@/workers/wait5s.js', import.meta.url)
-  //       );
-  //       worker.onmessage = function (event) {
-  //         console.log('Mensaje recibido:', event.data);
-  //         ref.current.classList.add(styles.close);
-  //       };
-  //       worker.postMessage('¡Hola Web Worker!');
-  //     };
-  //     if (document.readyState === 'complete') {
-  //       onPageLoad();
-  //     } else {
-  //       window.addEventListener('load', onPageLoad);
-  //       return () => {
-  //         window.removeEventListener('load', onPageLoad);
-  //         worker.terminate();
-  //       };
-  //     }
-  //   }, [ref]);
+  useEffect(() => {
+    const onPageLoad = () => {
+      const worker = new Worker(
+        new URL('@/workers/wait5s.js', import.meta.url)
+      );
+      worker.onmessage = function (event) {
+        console.log('Mensaje recibido:', event.data);
+        ref.current.classList.add(styles.close);
+      };
+      worker.postMessage('¡Hola Web Worker!');
+    };
+    if (document.readyState === 'complete') {
+      onPageLoad();
+    } else {
+      window.addEventListener('load', onPageLoad);
+      return () => {
+        window.removeEventListener('load', onPageLoad);
+        worker.terminate();
+      };
+    }
+  }, [ref]);
 
   //   useEffect(() => {
   //     const onPageLoad = () => {
