091f879128b185044c3f9ade1f33ee51f4a0e88f
asilva35 <sistev.contacto@gmail.com>
2023-06-03 13:11:04 -0500

    Delay 5s Loading Screen

--- components/LoadingScreens/gradient-waves/LSGradientWaves.js
+++ components/LoadingScreens/gradient-waves/LSGradientWaves.js
@@ -7,10 +7,10 @@ export default function LSGradientScreen({ image }) {
 
   useEffect(() => {
     const onPageLoad = () => {
-      //   setTimeout(() => {
-      //     ref.current.classList.add(styles.close);
-      //   }, 1500);
-      ref.current.classList.add(styles.close);
+      setTimeout(() => {
+        ref.current.classList.add(styles.close);
+      }, 5000);
+      //ref.current.classList.add(styles.close);
     };
     if (document.readyState === 'complete') {
       onPageLoad();
